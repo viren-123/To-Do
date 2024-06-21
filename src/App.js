8977ce4fcf0b22f0877f618bff641f85927657ea
viren-123 <122077717+viren-123@users.noreply.github.com>
2024-06-21 13:39:19 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ function App() {
         <p>
          ABHI
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
         
-        </a>
       </header>
       <Random/>
     </div>
