0c62ca5d039605e94701898c6a77e9c7fbb1c630
viren-123 <122077717+viren-123@users.noreply.github.com>
2024-06-21 14:08:58 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -5,13 +5,7 @@ import Random from './component/random';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         ABHI
-        </p>
-
-      </header>
+      
       <Random/>
 
     </div>
